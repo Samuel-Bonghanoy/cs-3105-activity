89f0e96d754e6fe691392566ae17ed1bb4ca4e13
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-09-02 20:38:30 +0800

    feat: initial commit

--- index.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  printf("why am i using this language its ass");
-  printf("because you have stockholm syndrome");
-}
---
+++ index.ts
@@ -0,0 +1,1 @@
+console.log("why are we here, just to suffer?");
